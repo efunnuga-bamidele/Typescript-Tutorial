84fc6b561efaf2663c9adf47cfce6f9f2f121b8b
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2023-05-05 07:40:03 +0100

    Interfaces

--- Basic_One/src/app.ts
+++ Basic_One/src/app.ts
@@ -1,3 +1,35 @@
+// Interfaces
+
+// Rules enforcer
+interface IsPerson{
+    name: string;
+    age: number;
+    speak(a: string): void;
+    spend(a: number): number;
+}
+
+
+const me: IsPerson = {
+    name: "shaun",
+    age: 30,
+    speak(text: string): void{
+        console.log(text);
+    },
+    spend(amount: number): number{
+        console.log('I spent', amount);
+        return amount;
+    }
+}
+
+console.log(me)
+
+const greetPerson = (person: IsPerson) => {
+    console.log('Hello', person.name);
+}
+
+greetPerson(me);
+
+
 import { Invoice } from "./modules/invoice.js";
 
 const invOne = new Invoice('Mario', 'work on the mario website', 250);
